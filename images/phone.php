87e97e0c20ef81ab5d891f3370488284497a2c0a
Rémi Scandella <remi.scandella@gmail.com>
2020-06-03 15:44:08 +0200

    Remove unused images

--- images/phone.php
+++
@@ -1,8 +0,0 @@
-<?php require_once 'phone-css.html' ?>
-
-<div class="phone">
-  <div class="phone-ear"></div>
-  <div class="phone-camera"></div>
-  <div class="phone-sensor"></div>
-  <div class="phone-screen"></div>
-</div>
