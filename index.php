7cc61a24d24c9c4f902b2303c8d6224d08cd4325
Rémi Scandella <remi.scandella@gmail.com>
2021-07-01 16:54:04 +0200

    Update blog link

--- index.php
+++ index.php
@@ -218,7 +218,7 @@ if ($css_critique_methode == 'push') {
           <?php $conditionBlog = true;
           if ($conditionBlog) { ?>
           <li>
-            <a href="/articles" data-section="blog" data-string="nav-articles"
+            <a href="/blog" data-section="blog" data-string="nav-articles"
               class="lien-interne lien-nav" <?=($start_section == 'blog') ? 'aria-current="page" tabindex="-1"' : ''?> style="--hue: 20">
               <?=$Textes->getString('nav-articles')?>
             </a>
