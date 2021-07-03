922f7724b72befdd6c16195b81dab0ea13a1f3a7
Rémi Scandella <remi.scandella@gmail.com>
2021-07-03 16:01:55 +0200

    Faster opacity transition on logo

--- pages/global-style.css.php
+++ pages/global-style.css.php
@@ -568,7 +568,8 @@ nav a[data-section="accueil"] {
   height: 100%;
   background-color: var(--link-color);
   opacity: 0;
-  transition: opacity .5s var(--easing-standard), background-color .2s var(--easing-standard);
+  transition: opacity .2s var(--easing-standard),
+              background-color .2s var(--easing-standard);
 }
 
 body[data-section="accueil"] .logo::after {
