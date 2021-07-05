71ba0fccc8c489be13f222ad8d4d3d6b611198ca
Rémi Scandella <remi.scandella@gmail.com>
2021-07-05 13:59:30 +0200

    Better shadow on selector

--- pages/global-style.css.php
+++ pages/global-style.css.php
@@ -1189,7 +1189,7 @@ theme-selector>.selector {
   background-color: var(--bg-color);
   box-shadow: 0 0 0 1px var(--link-color),
               0 0 0 2px var(--bg-color),
-              0 1px .2rem 1px var(--bg-color);
+              0 1px .2rem 3px var(--bg-color);
   margin-bottom: .6rem;
   border-radius: var(--border-radius);
   /*overflow: hidden;*/
