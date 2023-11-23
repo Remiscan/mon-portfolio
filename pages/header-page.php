8b63e49c75b895762e2bcc23923aa2e2b199dcd1
Rémi Scandella <remi.scandella@gmail.com>
2023-11-23 12:34:04 +0100

    Update social links

--- pages/header-page.php
+++ pages/header-page.php
@@ -19,19 +19,19 @@
   <div id="liens_contact" class="h4">
     <h3 class="h4"aria-hidden="true" hidden><?=$translation->get('me-contacter')?></h3>
 
-    <a href="mailto:contact@remiscan.fr" target="_blank" rel="noopener"
-       aria-label="<?=$translation->get('contact-email')?>"
-       class="expandable" style="--social-color:<?=$c_email->hsl()?>;">
-      <i class="svg">
-        <svg viewBox="0 0 24 24" id="svg-email"><use href="#email-closed" /></svg>
+    <a href="https://github.com/Remiscan" target="_blank" rel="noopener"
+       aria-label="<?=$translation->get('contact-github')?>"
+       class="expandable" style="--social-color:<?=$c_github->hsl()?>;">
+      <i class="svg" style="width: 1.4rem; height: 1.4rem;">
+        <svg viewBox="0 0 16 16"><use href="#github" /></svg>
       </i>
     </a>
 
-    <!--<a href="https://www.linkedin.com/in/remiscan" target="_blank" rel="noopener"
-           aria-label="<?=$translation->get('contact-linkedin')?>"
-           class="expandable" style="--social-color:<?=$c_linkedin->hsl()?>;">
-      <i class="svg" style="width: 1.25rem; height: 1.25rem;">
-        <svg viewBox="0 0 24 24"><use href="#linkedin" /></svg>
+    <!--<a href="https://codepen.io/remiscan" target="_blank" rel="noopener"
+       aria-label="<?=$translation->get('contact-codepen')?>"
+       class="expandable" style="--social-color:<?=$c_codepen->hsl()?>;">
+      <i class="svg" style="width: 1.5rem; height: 1.5rem;">
+        <svg viewBox="20 20 80 80"><use href="#codepen" /></svg>
       </i>
     </a>-->
 
@@ -43,19 +43,19 @@
       </i>
     </a>-->
 
-    <a href="https://github.com/Remiscan" target="_blank" rel="noopener"
-       aria-label="<?=$translation->get('contact-github')?>"
-       class="expandable" style="--social-color:<?=$c_github->hsl()?>;">
-      <i class="svg" style="width: 1.4rem; height: 1.4rem;">
-        <svg viewBox="0 0 16 16"><use href="#github" /></svg>
+    <a href="https://www.linkedin.com/in/remiscan" target="_blank" rel="noopener"
+           aria-label="<?=$translation->get('contact-linkedin')?>"
+           class="expandable" style="--social-color:<?=$c_linkedin->hsl()?>;">
+      <i class="svg" style="width: 1.25rem; height: 1.25rem;">
+        <svg viewBox="0 0 24 24"><use href="#linkedin" /></svg>
       </i>
     </a>
 
-    <a href="https://codepen.io/remiscan" target="_blank" rel="noopener"
-       aria-label="<?=$translation->get('contact-codepen')?>"
-       class="expandable" style="--social-color:<?=$c_codepen->hsl()?>;">
-      <i class="svg" style="width: 1.5rem; height: 1.5rem;">
-        <svg viewBox="20 20 80 80"><use href="#codepen" /></svg>
+    <a href="mailto:contact@remiscan.fr" target="_blank" rel="noopener"
+       aria-label="<?=$translation->get('contact-email')?>"
+       class="expandable" style="--social-color:<?=$c_email->hsl()?>;">
+      <i class="svg">
+        <svg viewBox="0 0 24 24" id="svg-email"><use href="#email-closed" /></svg>
       </i>
     </a>
   </div>
